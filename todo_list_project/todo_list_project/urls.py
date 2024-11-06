23c3bf6ee2739393a912a8008ad5308c0f65b0c5
GustavoRossii <161071605+GustavoRossii@users.noreply.github.com>
2024-11-06 15:05:18 -0300

    Update urls.py

--- todo_list_project/todo_list_project/urls.py
+++ todo_list_project/todo_list_project/urls.py
@@ -3,15 +3,15 @@ from django.urls import path, include
 from tasks import views
 
 urlpatterns = [
-    # Rota para o painel de administração do Django
+    
     path('admin/', admin.site.urls),
 
-    # Inclui as URLs padrão de autenticação do Django
+    
     path('accounts/', include('django.contrib.auth.urls')),
 
-    # URL para o registro de novos usuários
+   
     path('register/', views.register, name='register'),
 
-    # URL para a visualização da lista de tarefas; inclui todas as rotas do app tasks
+    
     path('tasks/', include('tasks.urls')),  # Certifique-se de que o tasks/urls.py está configurado corretamente
 ]
